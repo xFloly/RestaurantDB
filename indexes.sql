0ba99ae6e925ee0c7462586e5cde49cc0e5d8952
Filip Soszyński <101994826+xFloly@users.noreply.github.com>
2023-01-31 11:47:07 +0100

    Add files via upload

---
+++ indexes.sql
@@ -0,0 +1,19 @@
+create unique index Categories_pk
+on Categories(CategoryID)
+go
+
+create unique index Dishes_pk
+on Dishes(DishID)
+go
+
+create unique index Menu_pk
+on Menu(MenuPositionID)
+go
+
+create unique index Orders_pk
+on Orders(OrderID)
+go
+
+create unique index Customers_pk
+on Customers(CustomerID)
+go
